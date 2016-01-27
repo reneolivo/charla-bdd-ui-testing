be6e18af8bd4db1ce1cc7eec44c9300e47d359a6
René Olivo <minet.ws@gmail.com>
2016-01-26 20:35:59 -0400

    fix resolve navigation

--- test/e2e/features/demo.feature
+++
@@ -1,9 +0,0 @@
-Feature: Aurelia Skeleton App
-  As a user of the Skeleton App
-  I want to be able to go to the index page
-  And I want to see a main menu
-  So I can navigate to other sections
-
-Scenario: Navigating to the index page
-  Given I access "/welcome"
-  Then the title page title should be "Welcome | Aurelia"
--- test/e2e/features/step_definition/general.js
+++ test/e2e/features/step_definition/general.js
@@ -3,14 +3,20 @@ module.exports = function() {
     browser
       .loadAndWaitForAureliaPage('http://localhost:9000/#' + url)
       .then(function() {
-        return browser.waitForRouterComplete();
+        //Wait for router or resolve after 200ms timeout
+        return new Promise(function(resolve) {
+          setTimeout(function() { resolve(); }, 200);
+          browser
+            .waitForRouterComplete()
+            .then(function() { resolve(); });
+        });
       })
       .then(function() {
         cb();
       });
   });
 
-  this.Then(/^the title page title should be "([^"]*)"$/, function (title, callback) {
+this.Then(/^the page title should be "([^"]*)"$/, function (title, callback) {
     this.expect(browser.getTitle())
       .to.eventually.equal(title)
       .and.notify(callback);
